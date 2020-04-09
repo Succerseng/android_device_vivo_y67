c2e1784496a1541707614fc9a4e703f68c992f16
babq <1153791778@qq.com>
2020-04-09 16:29:51 +0800

    style(goodix): 字符移动
    *指纹bug依旧

--- goodix/Android.mk
+++ goodix/Android.mk
@@ -12,24 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(strip $(MTK_FINGERPRINT_SUPPORT)),yes)
 ifeq ($(strip $(MTK_FINGERPRINT_SELECT)),$(filter $(MTK_FINGERPRINT_SELECT), GF316M GF318M GF3118M GF518M GF5118M GF516M GF3208 GF5216))
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := fingerprint.$(TARGET_BOARD_PLATFORM)
-#LOCAL_PROPRIETARY_MODULE := true
-#LOCAL_MODULE_OWNER := mtk
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/goodix/public \
-    $(LOCAL_PATH)/goodix/gf_hal/public \
-    $(LOCAL_PATH)/goodix/goodixfingerprintd
-
-LOCAL_SRC_FILES := goodix/fingerprint.cpp
+    $(LOCAL_PATH)/include \
+    $(LOCAL_PATH)/gf_hal \
+    $(LOCAL_PATH)/goodixfingerprintd
+LOCAL_SRC_FILES := fingerprint.cpp
 
 LOCAL_SHARED_LIBRARIES := \
+    libgf_algo \
     libgf_hal \
     libgf_ca \
     liblog \
@@ -39,10 +37,8 @@ LOCAL_SHARED_LIBRARIES := \
     libgoodixfingerprintd_binder
 
 LOCAL_MODULE_TAGS := optional
-#include $(MTK_SHARED_LIBRARY)
 include $(BUILD_SHARED_LIBRARY)
 
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+
 endif
--- goodix/gf_hal/Android.mk
+++ goodix/gf_hal/Android.mk
@@ -17,13 +17,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libgf_hal
-#LOCAL_PROPRIETARY_MODULE := true
-#LOCAL_MODULE_OWNER := mtk
 LOCAL_SRC_FILES_64 := debug/arm64-v8a/libgf_hal.so
 LOCAL_SRC_FILES_32 := debug/armeabi-v7a/libgf_hal.so
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libgf_ca
+LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -32,12 +28,9 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libgf_algo
-#LOCAL_PROPRIETARY_MODULE := true
-#LOCAL_MODULE_OWNER := mtk
 LOCAL_SRC_FILES_64 := debug/arm64-v8a/libgf_algo.so
 LOCAL_SRC_FILES_32 := debug/armeabi-v7a/libgf_algo.so
-LOCAL_SHARED_LIBRARIES := \
-    libcutils
+LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
