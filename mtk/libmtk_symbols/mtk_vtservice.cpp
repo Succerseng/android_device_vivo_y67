de900b4994e42e2d708acf737c2ba67666733d34
succerseng <succerseng@gmail.com>
2022-05-05 16:34:48 +0800

    test:try fix vtservice it relate to ril

--- mtk/libmtk_symbols/mtk_vtservice.cpp
+++ mtk/libmtk_symbols/mtk_vtservice.cpp
@@ -8,4 +8,13 @@ extern "C" {
     void _ZN7android6Camera7connectEiRKNS_8String16Ei(){}
     void _ZN7android14ICameraService11asInterfaceERKNS_2spINS_7IBinderEEE(){}
     void _Z8sk_throwv(){}
+    void _ZNK8SkBitmap9eraseARGBEjjjj(){}
+    void _ZN7android12CameraSource16CreateFromCameraERKNS_2spINS_7ICameraEEERKNS1_INS_21ICameraRecordingProxyEEEiRKNS_8String16EjNS_4SizeEiRKNS1_INS_22IGraphicBufferProducerEEEb(){}
+    void _Z26getAdaptativeFrameRateInfo13_video_formatjjjPjS0_jjS0_S0_(){}
+    void _ZNK7android16NuMediaExtractor14getTrackFormatEjPNS_2spINS_8AMessageEEE(){}
+    void _ZN7android10CameraBaseINS_6CameraENS_12CameraTraitsIS1_EEE13getCameraInfoEiPNS_10CameraInfoE(){}
+    void _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEEPKcbPii(){}
+    void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjj(){}
+    void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_jiNS0_13transfer_typeEPK20audio_offload_info_tiiPK18audio_attributes_tb(){}
+    void _ZN7android2spIN9VTService9VTMALStubEED1Ev(){}
 }
