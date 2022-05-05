24bfc580b266ca5ba33bc9b9c49814e42f218155
succerseng <succerseng@gmail.com>
2022-05-05 19:29:48 +0800

    fix:seccomp
    device specific syscalls

--- device.mk
+++ device.mk
@@ -142,6 +142,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml \
+    frameworks/native/data/etc/android.hardware.camera.manual_sensor.xml:system/etc/permissions/android.hardware.camera.manual_sensor.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
--- mtk/libmtk_symbols/mtk_vtservice.cpp
+++ mtk/libmtk_symbols/mtk_vtservice.cpp
@@ -17,4 +17,5 @@ extern "C" {
     void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjj(){}
     void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_jiNS0_13transfer_typeEPK20audio_offload_info_tiiPK18audio_attributes_tb(){}
     void _ZN7android2spIN9VTService9VTMALStubEED1Ev(){}
+    void _ZN9VTService6VTCoreD0Ev(){}
 }
