ba68f1493b90c05a7b31723619c456c03b184f9e
succerseng <succerseng@gmail.com>
2022-05-07 20:37:35 +0800

    test:update patches
    add graphic patch

--- patches/install.sh
+++ patches/install.sh
@@ -8,7 +8,7 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying camera patches..."
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/camera_symbol.patch >> /dev/null 2&>1
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/camera_symbol.patch
 	echo " "
 done
 
@@ -18,7 +18,7 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying OTA patches..."
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/fix_updater_Operation_not_permitted.patch >> /dev/null 2&>1
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/fix_updater_Operation_not_permitted.patch
 	echo " "
 done
 
@@ -28,10 +28,10 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying wifi patches..."
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/fix_could_not_check_wifi_connection_status.patch >> /dev/null 2&>1
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/fix_could_not_check_wifi_connection_status.patch
 	echo ""
 	echo "Applying fingerprint patches..."
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/remain_mcRegistry.patch >> /dev/null 2&>1
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/remain_mcRegistry.patch
 	echo " "
 done
 
@@ -41,8 +41,18 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying camera patches..."
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/0009-add-mtk-color-format-support.patch >> /dev/null 2&>1
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/frameworks_av.patch >> /dev/null 2&>1
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/0009-add-mtk-color-format-support.patch
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/frameworks_av.patch
+	echo " "
+done
+
+dirs="frameworks/native"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying graphic patches..."
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/frameworks_native_ui.patch
 	echo " "
 done
 
@@ -51,8 +61,8 @@ dirs="system/core"
 for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
-	echo "Applying symbol patches..."
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/load_LD_SHIM_LIBS.patch >> /dev/null 2&>1
+	echo "Applying symbol and battery patches..."
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/system_core.patch
 	echo " "
 done
 
@@ -62,7 +72,7 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying sepolicy patches..."
-	git apply $rootdirectory/device/vivo/y67/patches/$dir/sepolicy_temporary_amend.patch >> /dev/null 2&>1
+	git apply $rootdirectory/device/vivo/y67/patches/$dir/sepolicy_temporary_amend.patch
 	echo " "
 done
 
